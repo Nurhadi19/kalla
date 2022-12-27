3ea7089403fa0071f91ddef8867d46b28a616c32
Rico Malangi <rikomalangi@gmail.com>
2022-12-27 23:25:37 +0800

    delete comment

--- application/controllers/Dashboard_Admin/Admin.php
+++ application/controllers/Dashboard_Admin/Admin.php
@@ -2,22 +2,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/userguide3/general/urls.html
-	 */
 	function __construct()
 	{
 		parent::__construct();
@@ -126,7 +110,7 @@ class Admin extends CI_Controller {
 		$config['last_tagl_close']  = '</span></li>';
 		$data['page'] = ($this->uri->segment(4)) ? $this->uri->segment(4) : 0;
 		
-		$data_prospek =  $this->m_admin->get_data($config["per_page"], $data['page'], $get_bulan, $get_nama);
+		$data_prospek =  $this->M_admin->get_data($config["per_page"], $data['page'], $get_bulan, $get_nama);
 		
 		if($get_bulan == null && $get_nama == null){
 			$config['total_rows'] = $this->db->count_all('tb_data_prospek');
